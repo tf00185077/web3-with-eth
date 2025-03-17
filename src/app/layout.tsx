b598238069d1fc836c2f16c3f209ee9173471964
Tim <tf00185077@gmail.com>
2025-03-17 22:01:39 +0800

    fix root and add not found page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,14 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import ThemeProvider from "../ui/theme-provider";
+import "./globals.css";
+import ThemeProvider from "./ui/theme-provider";
 import { SidebarProvider } from "@/components/ui/sidebar";
 import { MainLayoutSidebar } from "@/components/Sidebar";
 import { SessionProvider } from "next-auth/react";
 import { SidebarTrigger } from '@/components/ui/sidebar';
 import { WalletProvider } from "@/context/WalletContext";
 import WalletInformation from "@/components/WalletInformation";
+import PageSelector from "@/components/PageSelector";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -46,6 +47,7 @@ export default function RootLayout({
                 <SidebarTrigger />
                 <div className="p-4 flex flex-col gap-4 w-screen items-start justify-start">
                   <WalletInformation />
+                  <PageSelector />
                   {children}
                 </div>
               </SidebarProvider>
