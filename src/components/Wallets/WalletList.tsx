987f74c7b60e6aec509d50cbdc18b3033e8e1625
Tim <tf00185077@gmail.com>
2025-03-17 18:40:32 +0800

    change wallet to client component

--- src/components/Wallets/WalletList.tsx
+++ src/components/Wallets/WalletList.tsx
@@ -1,24 +1,14 @@
 import { getWalletList } from "./helper";
-import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
-import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button";
-import { Pencil } from "lucide-react";
+import { Suspense } from 'react';
+import WalletSelector from "./WalletSelector";
 export async function WalletList({ userId }: { userId: string; }) {
   if (!userId) {
     return <div>User ID is required</div>;
   }
   const walletList = await getWalletList(userId);
   return (
-    <RadioGroup className="flex flex-col gap-4 w-full" defaultValue={walletList[0].id.toString()}>
-      {walletList.map((wallet) => (
-        <div className="flex items-center space-x-2" key={wallet.id}>
-          <RadioGroupItem value={wallet.id.toString()} id={wallet.id.toString()} />
-          <Label className="cursor-pointer overflow-hidden text-ellipsis whitespace-nowrap  truncate" htmlFor={wallet.id.toString()}>{wallet.address}</Label>
-          <Button className="cursor-pointer" variant="outline" size="icon">
-            <Pencil className="w-4 h-4" />
-          </Button>
-        </div>
-      ))}
-    </RadioGroup>
+    <Suspense fallback={<div>Loading...</div>}>
+      <WalletSelector walletList={walletList} />
+    </Suspense>
   );
 }
---
+++ src/components/Wallets/WalletSelector.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import { RadioGroup, RadioGroupItem } from '@radix-ui/react-radio-group';
+import { Pencil } from 'lucide-react';
+import { useState, useEffect } from 'react';
+import { Button } from '../ui/button';
+import { Label } from '@radix-ui/react-label';
+import { Wallet } from '@prisma/client';
+
+export default function WalletSelector({ walletList }: { walletList: Wallet[]; }) {
+  const [walletAddress, setWalletAddress] = useState<string>('');
+  useEffect(() => {
+    console.log(walletAddress);
+  }, [walletAddress]);
+  return (
+    <RadioGroup
+      className="flex flex-col gap-4 w-full"
+      defaultValue={walletList[0].id.toString()}
+      onValueChange={(value) => { setWalletAddress(value); }}
+    >
+      {walletList.map((wallet, index) => (
+        <div
+          className="cursor-pointer flex items-center space-x-2 hover:border hover:border-gray-300 px-2 py-1 rounded-md data-[state=checked]:border-gray-300 data-[state=checked]:border-1"
+          key={wallet.address}
+          data-state={wallet.address === walletAddress ? "checked" : "unchecked"}
+        >
+          <RadioGroupItem value={wallet.address} id={wallet.address} />
+          <Label
+            className="flex-1 overflow-hidden text-ellipsis whitespace-nowrap truncate cursor-pointer"
+            htmlFor={wallet.address}
+          >
+            {wallet.wallet_nickname ? wallet.wallet_nickname : `Your Wallet ${index + 1}`}
+          </Label>
+          <Button className="cursor-pointer" variant="outline" size="icon">
+            <Pencil className="w-4 h-4" />
+          </Button>
+        </div>
+      ))}
+    </RadioGroup>
+  );
+}
